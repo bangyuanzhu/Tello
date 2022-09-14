b27b7e7ae5162818a58a5ab9b16e8dae66c27a4c
ngwaidoong <98247129+ngwaidoong@users.noreply.github.com>
2022-09-14 14:06:41 +0800

    Add files via upload

---
+++ imagecapture.py
@@ -0,0 +1,17 @@
+from djitellopy import tello
+import cv2
+
+
+me = tello.Tello()
+me.connect()                        #Connect to the tello. Make sure to connect device via wifi first.
+print(me.get_battery())             #Gets battery percent
+
+
+#Streams image data from the tello camera
+me.streamon()
+
+while True:
+    img = me.get_frame_read().frame
+    img = cv2.resize(img, (360, 240))#Resizes the image to 360p x 240p to minimise data processing load
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
